c41946952bf57603f343ad64f8b35ab907c0d4e0
Zac Hester <zac.hester@gmail.com>
2015-02-25 11:32:12 -0600

    Roughed-in some ideas on providing query input validation.

--- Util/StructuredQuery.php
+++ Util/StructuredQuery.php
@@ -109,7 +109,6 @@ class StructuredQuery {
         //symbols that must be quoted
         $patterns = [];
         foreach( self::$lexicon as $sym => $spec ) {
-            //$patterns[] = '(?P<' . $sym . '>' . $spec[ 0 ] . ')';
             $patterns[] = '(' . $spec[ 0 ] . ')';
             if( $spec[ 1 ] == true ) {
                 $this->quote_syms[] = $sym;
@@ -183,6 +182,57 @@ class StructuredQuery {
     }
 
 
+    /**
+     * Executes validation on a structured query field based on a set of
+     * rules.
+     *
+     * Rules specify:
+     *   - scalar type or vector
+     *   - min/max values/sizes
+     *   - validation expression
+     *
+     * A single rule is specified as an array:
+     *
+     *      [ <scalar type or vector>, <req>, <min>, <max>, <expr> ]
+     *
+     * The <min>, <max>, and <expr> items change their meaning based on the
+     * type of scalar, or if it's a vector.
+     *
+     * <scalar type or vector> must be one of the following strings:
+     * keyword, integer, float, string, vector
+     *
+     * The <req> item just indicates if this item is required or optional.
+     * True means it's required.  False means it's optional.
+     *
+     * <min> and <max> evaluations per type:
+     *
+     * keyword: 00: normal, 01: no nulls, 10: no false, 11: no true
+     * integer: minimum allowed value, maximum allowed value
+     * float: minimum allowed value, maximum allowed value
+     * string: minimum allowed length, maximum allowed length
+     * vector: minimum allowed length, maximum allowed length
+     *
+     * The <expr> item allows a few other validation checks:
+     * - if it's a string, use a regular expression (scalars only) to test
+     * - if it's a function, call the function that receives the value (return
+     *   true = valid)
+     * - if it's an array, do any of the previous with each item in the array
+     *
+     * Rules are structured to map to the same positions in the structured
+     * data for the field.  If the field is a scaler, only one rule is needed.
+     * If the field contains a vector, a vector of rules is needed.  Nesting
+     * works as expected.
+     *
+     * @param key   The field's name in the query to test
+     * @param rules An array of rules to use to check the value
+     * @return      True if the field passes all validation checks
+     */
+    public function validate( $key, $rules ) {
+
+        ////ZIH - left off here
+    }
+
+
     /*------------------------------------------------------------------------
     Protected Methods
     ------------------------------------------------------------------------*/
@@ -205,6 +255,57 @@ class StructuredQuery {
             return '"' . $matches[ $index ] . '"';
         }
         return $match;
+    }
+
+
+    /**
+     * Performs validation for a given value based on the given rule.
+     *
+     * @param value The value to validate
+     * @param rule  The rule to use to check the value
+     * @return      True if the value is valid, otherwise false
+     */
+    private function validate_value( $value, $rule ) {
+
+        //base validation on type of rule
+        switch( $rule[ 0 ] ) {
+            case 'vector':
+
+                //for vectors, the <req> item might contain an array of rules
+                if( $rule[ 1 ] !== false ) {
+
+                    //not enough rules to validate what was given
+                    if( count( $rule[ 1 ] ) < count( $value ) ) {
+                        return false;
+                    }
+
+                    //check each value against each rule
+                    for( $i = 0; $i < count( $value ); ++$i ) {
+                        $this->validate_value( $value[ $i ], $rule[ 1 ][ $i ] );
+                    }
+                }
+
+                break;
+
+            case 'keyword':
+                ////ZIH
+                break;
+            case 'integer':
+                ////ZIH
+                break;
+            case 'float':
+                ////ZIH
+                break;
+            case 'string':
+                break;
+            default:
+                throw new \RuntimeException(
+                    "Invalid validation rule type: {$rule[0]}."
+                );
+                break;
+        }
+
+        ////ZIH - left off here
     }
 
 }
